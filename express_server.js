703cd6cbcf275bcde881689f9c0e9d954e75ee57
Anthony <anthonyzhu132@gmail.com>
2019-09-02 23:48:54 +0000

    added app.post method to utiliz the form that was made in urls_new.ejs and then sends back OK to confirm that the object was received

--- express_server.js
+++ express_server.js
@@ -1,8 +1,14 @@
+const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
 const port = 8080; // default port 8080
-
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+
+function generateRandomString() {
+  
+};
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -29,6 +35,11 @@ app.get("/urls", (request, response) => {
 app.get("/urls/:shortURL", (request, response) => {
   let templateVars = { shortURL: request.params.shortURL, longURL: request.params.longURL};
   response.render("urls_show", templateVars);
+});
+
+app.post("/urls", (request, response) => {
+  console.log(request.body); // Log the POST request body to the console
+  response.send("Ok!"); // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(port, () => {
